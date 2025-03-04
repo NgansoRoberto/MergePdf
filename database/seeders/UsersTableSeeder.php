73ce8cf9ca27d653a5bfaa8035e6075281b821e2
Nganso Roberto <robertonganso017@gmail.com>
2025-03-04 10:20:55 +0100

    Mise en ligne du Projet

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now()
+        ]);
+
+        // Add more users if needed
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now()
+        ]);
+    }
+}
